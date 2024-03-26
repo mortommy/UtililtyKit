76a6a17342042180997475fc3ebbef59948ca0f4
mortommy <tommaso.morello@gmail.com>
2024-03-26 23:15:22 +0100

    add date to csv archived file

--- utilitykit/service_factory_filesystem.py
+++ utilitykit/service_factory_filesystem.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 import shutil
 import zipfile
@@ -16,6 +17,12 @@ def move_file_to_zip_archive(file_name: str, sub_folder: str):
         downloads_folder = os.path.join(os.getcwd(), sub_folder)
         if not os.path.exists(downloads_folder):
             os.makedirs(downloads_folder)
+
+        current_date = dt.datetime.now().strftime("%Y-%m-%d")
+        csv_file_name = os.path.basename(file_name)
+        csv_file_name_with_date = f'{os.path.splitext(csv_file_name)[0]}_{current_date}.csv'
+        csv_file_path = os.path.join(os.path.dirname(file_path), csv_file_name_with_date)
+        os.rename(file_path, csv_file_path)
 
         zip_file_name = os.path.splitext(os.path.basename(file_name))[0] + '.zip'
         zip_file_path = os.path.join(downloads_folder, zip_file_name)
